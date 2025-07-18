61037e6b0417a0a7a988856329bf18f209d9fadc
Mapaor <135515260+Mapaor@users.noreply.github.com>
2025-07-18 09:36:24 +0200

    seo and meta tags

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,19 @@ export default function RootLayout({ children }) {
     <html lang="ca">
       <head>
         <title>Horaris Rodalies</title>
+        <meta name="description" content="Consulta fàcilment els horaris de la línia R11 de Rodalies entre Barcelona, Girona, Flaçà i Bordils. Trens rodalies, mitja distància (MD) i AVE. Rodalies de Catalunya, API oficial de la Generalitat i API descoberta de Renfe." />
         <link rel="icon" href="/favicon.ico" />
+        {/* Open Graph / WhatsApp / Facebook */}
+        <meta property="og:title" content="Horaris Rodalies R11" />
+        <meta property="og:description" content="Consulta fàcilment els horaris de la línia R11 de Rodalies entre Barcelona, Girona, Flaçà i Bordils. Trens rodalies, mitja distància (MD) i AVE. Rodalies de Catalunya, API oficial de la Generalitat i API descoberta de Renfe." />
+        <meta property="og:image" content="https://rodalies.lol/r11-foto.png" />
+        <meta property="og:url" content="https://rodalies.lol" />
+        <meta property="og:type" content="website" />
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Horaris Rodalies R11" />
+        <meta name="twitter:description" content="Consulta fàcilment els horaris de la línia R11 de Rodalies entre Barcelona, Girona, Flaçà i Bordils. Trens rodalies, mitja distància (MD) i AVE. Rodalies de Catalunya, API oficial de la Generalitat i API descoberta de Renfe." />
+        <meta name="twitter:image" content="https://rodalies.lol/r11-foto.png" />
       </head>
       <body>
         {children}
