a4e336a62359ad3d824c879fa816632599c963aa
Mapaor <135515260+Mapaor@users.noreply.github.com>
2025-07-18 09:23:51 +0200

    favicon

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ export default function RootLayout({ children }) {
     <html lang="ca">
       <head>
         <title>Horaris Rodalies</title>
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
         {children}
